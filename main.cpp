e2a2531fc5dee56eb9a86f2443a5262571d962c7
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2023-04-16 16:03:50 -0400

    create usb2serial

--- main.cpp
+++ main.cpp
@@ -6,36 +6,17 @@
 int main()
 {
     stdio_init_all();
-    uart_init(uart1, 115200);
-
-    gpio_set_function(4, GPIO_FUNC_UART);
-    gpio_set_function(5, GPIO_FUNC_UART);
-
-    sleep_ms(5000);
-
-    uart_puts(uart1, "AT+MQTTUSERCFG=0,1,\"rp2040\",\"\",\"\",0,0,\"\"\r\n");
-    sleep_ms(100);
-    uart_puts(uart1, "AT+MQTTCONNCFG=0,0,0,\"home/nodes/sensor/rp2040/status\",\"offline\",0,0\r\n");
-    sleep_ms(100);
-    uart_puts(uart1, "AT+MQTTCONN=0,\"10.0.0.167\",1883,0\r\n");
-    sleep_ms(100);
-    uart_puts(uart1, "AT+MQTTPUB=0,\"home/nodes/sensor/rp2040/status\",\"online\",0,0\r\n");
-    sleep_ms(100);
 
     while (true)
     {
         int char_usb = getchar_timeout_us(1);
         if (char_usb != PICO_ERROR_TIMEOUT)
         {
-            uart_putc_raw(uart1, char_usb);
-            if (char_usb == '\r')
-            {
-                uart_putc_raw(uart1, '\n');
-            }
+            uart_putc_raw(uart0, char_usb);
         }
-        if (uart_is_readable(uart1))
+        if (uart_is_readable(uart0))
         {
-            putchar_raw(uart_getc(uart1));
+            putchar_raw(uart_getc(uart0));
         }
     }
     return 0;
